c525a8130e2b6c673b101705afe64759d6d0fc64
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2023-05-09 17:06:43 -0300

    fix: Set Page Size To 2

--- SIRI_BACK/settings.py
+++ SIRI_BACK/settings.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 2,
 }
 
 LANGUAGE_CODE = "en-us"
