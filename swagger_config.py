f96c3d071e685a6bd2f7ed89bb80010020d74c55
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2023-05-11 14:45:46 -0300

    fix: Linting Problems

--- swagger_config.py
+++ swagger_config.py
@@ -5,7 +5,8 @@ schema_view = get_schema_view(
     openapi.Info(
         title="SIRI",
         default_version="v1",
-        description="SIRI is a Django-based project developed for DPESC, aimed at efficiently managing internal processes within the organization.",
+        description="SIRI is a Django-based project developed for DPESC, aimed at efficiently managing "
+        "internal processes within the organization.",
         terms_of_service="",
         contact=openapi.Contact(email="suporte-getig@defensoria.sc.gov.br"),
         license=openapi.License(name=""),
