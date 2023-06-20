57bf75776efb447f4088abf8fbf3ea1fcb4df61a
Otavio-PC <otavio.aspc@gmail.com>
2023-06-20 13:51:02 -0300

    fixed date filter

--- SIRI_BACK/tasks.py
+++ SIRI_BACK/tasks.py
@@ -10,10 +10,10 @@ from stock.models import Protocol
 
 @shared_task
 def verify_end_date():
-    today = datetime.now()
+    today = datetime.now().date()
     ninety_days_later = today + timedelta(days=90)
 
-    protocols = Protocol.objects.filter(end_date__lte=ninety_days_later)
+    protocols = Protocol.objects.filter(end_date__date=ninety_days_later)
 
     admins = User.objects.filter(is_superuser=True)
 
@@ -21,13 +21,13 @@ def verify_end_date():
 
     from_email = os.environ.get("EMAIL_HOST_USER")
 
-    protocol_codes = [protocol.code for protocol in protocols]
+    protocol_codes = [protocol.code for protocol i  n protocols]
 
     protocol_codes_str = "\n".join(protocol_codes)
 
     send_mail(
         "Alerta Final de Vingência de Ata",
-        f"Os seguintes protocolos estão a três dias do final de suas vingências:\n{protocol_codes_str}",
+        f"Os seguintes protocolos estão a noventa dias do final de suas vingências:\n{protocol_codes_str}",
         from_email,
         recipient_list,
         fail_silently=False,
