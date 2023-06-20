dbc11c63048bd649ca2ed802a6066d07ee8863ab
Otavio-PC <otavio.aspc@gmail.com>
2023-06-20 12:44:24 -0300

    lint done, verify task daily

--- SIRI_BACK/settings.py
+++ SIRI_BACK/settings.py
@@ -124,13 +124,13 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-CELERY_IMPORTS = ('SIRI_BACK.tasks',)
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_IMPORTS = ("SIRI_BACK.tasks",)
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 CELERY_BEAT_SCHEDULE = {
-    'verify_end_date': {
-        'task': 'SIRI_BACK.tasks.verify_end_date',
-        'schedule': crontab(minute=0, hour=0),
+    "verify_end_date": {
+        "task": "SIRI_BACK.tasks.verify_end_date",
+        "schedule": crontab(minute=0, hour=0),
     },
 }
--- SIRI_BACK/tasks.py
+++ SIRI_BACK/tasks.py
@@ -1,30 +1,33 @@
-from celery import shared_task
-from django.core.mail import send_mail
-from django.contrib.auth.models import User
-from datetime import datetime, timedelta
-from stock.models import Protocol
 import os
+from datetime import datetime, timedelta
+
+from celery import shared_task
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+
+from stock.models import Protocol
+
 
 @shared_task
 def verify_end_date():
     today = datetime.now()
     ninety_days_later = today + timedelta(days=90)
-    
+
     protocols = Protocol.objects.filter(end_date__lte=ninety_days_later)
-    
+
     admins = User.objects.filter(is_superuser=True)
-    
-    recipient_list = admins.values_list('email', flat=True)
-    
+
+    recipient_list = admins.values_list("email", flat=True)
+
     from_email = os.environ.get("EMAIL_HOST_USER")
 
     protocol_codes = [protocol.code for protocol in protocols]
 
-    protocol_codes_str = '\n'.join(protocol_codes)
+    protocol_codes_str = "\n".join(protocol_codes)
 
     send_mail(
-        'Alerta Final de Vingência de Ata',
-        f'Os seguintes protocolos estão a três dias do final de suas vingências:\n{protocol_codes_str}',
+        "Alerta Final de Vingência de Ata",
+        f"Os seguintes protocolos estão a três dias do final de suas vingências:\n{protocol_codes_str}",
         from_email,
         recipient_list,
         fail_silently=False,
