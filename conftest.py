acd51eb8c9cc8075db85d034a48e95b53d4b8f5f
Otavio-PC <otavio.aspc@gmail.com>
2023-06-29 15:50:03 -0300

    conftest && pytest fixed, test for stock models done

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
+from datetime import date, datetime
 
 from order.models import Order, OrderItem
 from stock.models import (
@@ -10,34 +11,48 @@ from stock.models import (
     Sector,
     Stock,
     StockItem,
+    Supplier,
+    DispatchReport,
+    AccountantReport,
+    CategoryBalance,
+    Protocol,
+    Invoice,
+    ReceivingReport,
+    BiddingExemption,
+    ProtocolItem,
 )
 from user.models import Client
 
 
 @pytest.fixture
-def measure():
-    return Measure.objects.create(name="test_measure")
-
-
-@pytest.fixture
 def public_defense():
     return PublicDefense.objects.create(
-        name="test_public_defense", district="test_district", address="test_address"
+        name="Test Public Defense", district="Test District", address="Test Address"
     )
 
 
 @pytest.fixture
 def sector(public_defense):
-    return Sector.objects.create(name="test_sector", public_defense=public_defense)
+    return Sector.objects.create(name="Test Sector", public_defense=public_defense)
+
+
+@pytest.fixture
+def stock(sector):
+    return Stock.objects.create(
+        sector=sector,
+    )
 
 
 @pytest.fixture
 def category(sector):
-    category = Category.objects.create(
-        name="test_category", slug="test_category_slug", is_standard=True
-    )
-    category.sector.add(sector)
+    category = Category.objects.create(name="Test Category", code="Test Code")
+    category.sector.set([sector])
     return category
+
+
+@pytest.fixture
+def measure():
+    return Measure.objects.create(name="Test Measure")
 
 
 @pytest.fixture
@@ -45,14 +60,110 @@ def product(category, measure):
     return Product.objects.create(
         category=category,
         measure=measure,
-        name="test_product",
-        slug="test_product_slug",
+        price=10.0,
+        name="Test Product",
+        description="Test Description",
+        code="Test Code",
+        is_available=True,
     )
 
 
 @pytest.fixture
-def stock(sector):
-    return Stock.objects.create(sector=sector)
+def stock_item(stock, product):
+    return StockItem.objects.create(stock=stock, product=product, quantity=0)
+
+
+@pytest.fixture
+def supplier(category):
+    supplier = Supplier.objects.create(
+        name="Test Supplier",
+        agent="Test Agent",
+        address="Test Address",
+        email="test@example.com",
+        phone="1234567890",
+        ein="12345",
+        ssn="54321",
+        nic="98765",
+    )
+    supplier.category.set([category])
+    return supplier
+
+
+@pytest.fixture
+def protocol(supplier, category):
+    return Protocol.objects.create(
+        code="Test Code",
+        supplier=supplier,
+        category=category,
+        start_date=datetime(2023, 6, 1, 10, 0, 0),
+        end_date=datetime(2023, 6, 2, 15, 30, 0),
+    )
+
+
+@pytest.fixture
+def protocol_item(protocol, product):
+    return ProtocolItem.objects.create(
+        protocol=protocol, product=product, original_quantity=0, quantity=0
+    )
+
+
+@pytest.fixture
+def invoice(supplier, public_defense):
+    return Invoice.objects.create(
+        supplier=supplier,
+        public_defense=public_defense,
+        code="Test Code",
+        total_value=10.00,
+    )
+
+
+@pytest.fixture
+def receiving_report(product, stock_item, supplier):
+    return ReceivingReport.objects.create(
+        product=product,
+        stock_item=stock_item,
+        quantity=10,
+        supplier=supplier,
+        file="Test File",
+        description="Test Description",
+    )
+
+
+@pytest.fixture
+def dispatch_report(product, stock_item, public_defense):
+    return DispatchReport.objects.create(
+        product=product,
+        stock_item=stock_item,
+        quantity=0,
+        public_defense=public_defense,
+        file="Test File",
+        description="Test Description",
+    )
+
+
+@pytest.fixture
+def bidding_exemption(product, stock, invoice):
+    return BiddingExemption.objects.create(
+        product=product, stock=stock, invoice=invoice, quantity=0
+    )
+
+
+@pytest.fixture
+def accountant_report():
+    return AccountantReport.objects.create(
+        month="June 2023",
+        total_previous_value=1000.0,
+        total_entry_value=500.0,
+        total_output_value=200.0,
+        total_current_value=1300.0,
+    )
+
+
+@pytest.fixture
+def category_balance(category):
+    return CategoryBalance.objects.create(
+        category=category, date=date.today(), balance=1500.0
+    )
 
 
 @pytest.fixture
@@ -70,8 +181,3 @@ def order(requester):
 @pytest.fixture
 def order_item(order, product):
     return OrderItem.objects.create(order=order, product=product)
-
-
-@pytest.fixture
-def stock_item(stock, product):
-    return StockItem.objects.create(stock=stock, product=product, current_quantity=2)
---
+++ stock/test_models.py
@@ -0,0 +1,123 @@
+import pytest
+
+
+from .models import (
+    AccountantReport,
+    BiddingExemption,
+    Category,
+    CategoryBalance,
+    DispatchReport,
+    Invoice,
+    Measure,
+    Product,
+    Protocol,
+    ProtocolItem,
+    PublicDefense,
+    ReceivingReport,
+    Sector,
+    Stock,
+    StockItem,
+    Supplier,
+)
+
+
+@pytest.mark.django_db
+def test_public_defense(public_defense):
+    assert isinstance(public_defense, PublicDefense)
+    assert str(public_defense) == "Test Public Defense"
+
+
+@pytest.mark.django_db
+def test_sector(sector):
+    assert isinstance(sector, Sector)
+    assert str(sector) == "Test Sector"
+
+
+@pytest.mark.django_db
+def test_stock(stock):
+    assert isinstance(stock, Stock)
+    assert str(stock) == f"{stock.sector} - {stock.sector.public_defense}"
+
+
+@pytest.mark.django_db
+def test_category(category):
+    assert isinstance(category, Category)
+    assert str(category) == "Test Category"
+
+
+@pytest.mark.django_db
+def test_measure(measure):
+    assert isinstance(measure, Measure)
+    assert str(measure) == "Test Measure"
+
+
+@pytest.mark.django_db
+def test_product(product):
+    assert isinstance(product, Product)
+    assert str(product) == "Test Product"
+
+
+@pytest.mark.django_db
+def test_stock_item(stock_item):
+    assert isinstance(stock_item, StockItem)
+    assert (
+        str(stock_item)
+        == f"{stock_item.stock.sector} - {stock_item.stock.sector.public_defense} - {stock_item.product}"
+    )
+
+
+@pytest.mark.django_db
+def test_supplier(supplier):
+    assert isinstance(supplier, Supplier)
+    assert str(supplier) == "Test Supplier"
+
+
+@pytest.mark.django_db
+def test_protocol(protocol):
+    assert isinstance(protocol, Protocol)
+    assert str(protocol) == "Test Code"
+
+
+@pytest.mark.django_db
+def test_protocol_item(protocol_item):
+    assert isinstance(protocol_item, ProtocolItem)
+    assert (
+        str(protocol_item)
+        == f"{protocol_item.protocol} - {protocol_item.protocol.supplier} - {protocol_item.product}"
+    )
+
+
+@pytest.mark.django_db
+def test_invoice(invoice):
+    assert isinstance(invoice, Invoice)
+    assert str(invoice) == "Test Code"
+
+
+@pytest.mark.django_db
+def test_receiving_report(receiving_report):
+    assert isinstance(receiving_report, ReceivingReport)
+    assert str(receiving_report) == f"ReceivingReport {receiving_report.id}"
+
+
+@pytest.mark.django_db
+def test_dispatch_report(dispatch_report):
+    assert isinstance(dispatch_report, DispatchReport)
+    assert str(dispatch_report) == f"DispatchReport {dispatch_report.id}"
+
+
+@pytest.mark.django_db
+def test_bidding_exemption(bidding_exemption):
+    assert isinstance(bidding_exemption, BiddingExemption)
+    assert str(bidding_exemption) == f"BiddingExemption {bidding_exemption.id}"
+
+
+@pytest.mark.django_db
+def test_accountant_report(accountant_report):
+    assert isinstance(accountant_report, AccountantReport)
+    assert str(accountant_report) == f"AccountantReport {accountant_report.id}"
+
+
+@pytest.mark.django_db
+def test_category_balance(category_balance):
+    assert isinstance(category_balance, CategoryBalance)
+    assert str(category_balance) == f"CategoryBalance {category_balance.id}"
